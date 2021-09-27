252fe9c88c7d81e59c5563cbbd488703e4ecb97a
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-27 13:34:46 -0500

    Create Q3Code.java

---
+++ Homework2/Q3Code.java
@@ -0,0 +1,19 @@
+findPoison(int[] arr, int x) //x is the poison
+
+int size = sizeof(arr);
+if(size > 1){
+
+int middle = n/2
+if(arr[middle] == x){
+   return middle;
+}
+int[] bottom = [1,...,middle]
+int[] top = [middle+1,...,n]
+
+if(poison = in bottom){
+  return findPoison(bottom, x);
+}else{
+  return findPoison(top, x);
+} 
+           
+}
